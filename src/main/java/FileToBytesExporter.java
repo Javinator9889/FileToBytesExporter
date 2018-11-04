e6fc70150480989bf0b9eae19235018a7137b7e1
Javinator9889 <javialonso007@hotmail.es>
2018-11-04 12:43:33 +0100

    Included Cloneable and Serializable interfaces

--- src/main/java/FileToBytesExporter.java
+++ src/main/java/FileToBytesExporter.java
@@ -11,13 +11,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 /**
  * <p>Files to bytes exporter provides a <b>custom, fast</b> class for generating <i>binary
- * files</i> from plain (or not) text files.<br /><br />
- *
+ * files</i> from plain (or not) text files.
+ * <p>
  * Binary files are present at <b>every computer</b> and the have some interesting advantages facing
  * regular text files:
  * <ul>
@@ -45,14 +46,13 @@ import java.util.stream.IntStream;
  * to others to inspect and know the <i>file contents</i> without a proper editor. Also they need to
  * know <b>which type of data structure</b> is saved for obtaining the desired instance.
  * </li>
- * </ul><br /><br />
+ * </ul><p>
  *
  * This class allows you to easily generate binary files from regular text files with some simple
  * methods and attributes. In addition, it supports <b>glob syntax</b>, so it is even easier to find
  * and convert lots of files with just one line.
- * </i></p>
  */
-public class FileToBytesExporter {
+public class FileToBytesExporter implements Cloneable, Serializable {
     private String mFilename;
     private ArrayList<String> mPath;
     private String mReadData;
@@ -90,10 +90,23 @@ public class FileToBytesExporter {
      * @see String
      */
     public FileToBytesExporter(String filename, String... paths) {
+        this(filename, new ArrayList<>(Arrays.asList(paths)), null, null);
+    }
+
+    /**
+     * Private constructor for cloning of generating a new instance - only visible for this class
+     *
+     * @param filename      source file
+     * @param paths         list of paths where search for the source file
+     * @param readData      current read data
+     * @param fileSeparator current used file separator
+     */
+    private FileToBytesExporter(String filename, ArrayList<String> paths, String readData,
+                                String fileSeparator) {
         mFilename = filename;
-        mPath = new ArrayList<>(Arrays.asList(paths));
-        mReadData = null;
-        mFileSeparator = null;
+        mPath = paths;
+        mReadData = readData;
+        mFileSeparator = fileSeparator;
     }
 
     /**
@@ -351,7 +364,7 @@ public class FileToBytesExporter {
      * MessageDigest#digest(byte[])} method, generating a {@link MessageDigest#getInstance(String)
      * SHA-256 hash}. If it fails because {@link NoSuchAlgorithmException no SHA-256 hashing
      * algorithm} was found, it just returns the {@link String#hashCode()} casted to {@code
-     * String}.<br /><br />
+     * String}.<p>
      *
      * You can {@code @Override} this method if you inherit from {@code FileToBytesExporter} if you
      * need any other {@link StandardCharsets} charset.
@@ -369,6 +382,165 @@ public class FileToBytesExporter {
         } catch (NoSuchAlgorithmException ignored) {
             return String.valueOf(source.hashCode());
         }
+    }
+
+    /**
+     * Indicates whether some other object is "equal to" this one.
+     * <p>
+     * The {@code equals} method implements an equivalence relation on non-null object references:
+     * <ul>
+     * <li>It is <i>reflexive</i>: for any non-null reference value
+     * {@code x}, {@code x.equals(x)} should return {@code true}.
+     * <li>It is <i>symmetric</i>: for any non-null reference values
+     * {@code x} and {@code y}, {@code x.equals(y)} should return {@code true} if and only if {@code
+     * y.equals(x)} returns {@code true}.
+     * <li>It is <i>transitive</i>: for any non-null reference values
+     * {@code x}, {@code y}, and {@code z}, if {@code x.equals(y)} returns {@code true} and {@code
+     * y.equals(z)} returns {@code true}, then {@code x.equals(z)} should return {@code true}.
+     * <li>It is <i>consistent</i>: for any non-null reference values
+     * {@code x} and {@code y}, multiple invocations of {@code x.equals(y)} consistently return
+     * {@code true} or consistently return {@code false}, provided no information used in {@code
+     * equals} comparisons on the objects is modified.
+     * <li>For any non-null reference value {@code x},
+     * {@code x.equals(null)} should return {@code false}.
+     * </ul>
+     * <p>
+     * The {@code equals} method for class {@code Object} implements the most discriminating
+     * possible equivalence relation on objects; that is, for any non-null reference values {@code
+     * x} and {@code y}, this method returns {@code true} if and only if {@code x} and {@code y}
+     * refer to the same object ({@code x == y} has the value {@code true}).
+     * <p>
+     * Note that it is generally necessary to override the {@code hashCode} method whenever this
+     * method is overridden, so as to maintain the general contract for the {@code hashCode} method,
+     * which states that equal objects must have equal hash codes.
+     *
+     * @param o the reference object with which to compare.
+     *
+     * @return {@code true} if this object is the same as the obj argument; {@code false} otherwise.
+     *
+     * @see #hashCode()
+     * @see java.util.HashMap
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileToBytesExporter exporter = (FileToBytesExporter) o;
+        return Objects.equals(mFilename, exporter.mFilename) &&
+                Objects.equals(mPath, exporter.mPath) &&
+                Objects.equals(mReadData, exporter.mReadData) &&
+                Objects.equals(mFileSeparator, exporter.mFileSeparator) &&
+                Objects.equals(lock, exporter.lock);
+    }
+
+    /**
+     * Returns a hash code value for the object. This method is supported for the benefit of hash
+     * tables such as those provided by {@link java.util.HashMap}.
+     * <p>
+     * The general contract of {@code hashCode} is:
+     * <ul>
+     * <li>Whenever it is invoked on the same object more than once during
+     * an execution of a Java application, the {@code hashCode} method must consistently return the
+     * same integer, provided no information used in {@code equals} comparisons on the object is
+     * modified. This integer need not remain consistent from one execution of an application to
+     * another execution of the same application.
+     * <li>If two objects are equal according to the {@code equals(Object)}
+     * method, then calling the {@code hashCode} method on each of the two objects must produce the
+     * same integer result.
+     * <li>It is <em>not</em> required that if two objects are unequal
+     * according to the {@link java.lang.Object#equals(java.lang.Object)} method, then calling the
+     * {@code hashCode} method on each of the two objects must produce distinct integer results.
+     * However, the programmer should be aware that producing distinct integer results for unequal
+     * objects may improve the performance of hash tables.
+     * </ul>
+     * <p>
+     * As much as is reasonably practical, the hashCode method defined by class {@code Object} does
+     * return distinct integers for distinct objects. (The hashCode may or may not be implemented as
+     * some function of an object's memory address at some point in time.)
+     *
+     * @return a hash code value for this object.
+     *
+     * @see java.lang.Object#equals(java.lang.Object)
+     * @see java.lang.System#identityHashCode
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(mFilename, mPath, mReadData, mFileSeparator, lock);
+    }
+
+    /**
+     * Creates and returns a copy of this object.  The precise meaning of "copy" may depend on the
+     * class of the object. The general intent is that, for any object {@code x}, the expression:
+     * <blockquote>
+     * <pre>
+     * x.clone() != x</pre></blockquote>
+     * will be true, and that the expression:
+     * <blockquote>
+     * <pre>
+     * x.clone().getClass() == x.getClass()</pre></blockquote>
+     * will be {@code true}, but these are not absolute requirements. While it is typically the case
+     * that:
+     * <blockquote>
+     * <pre>
+     * x.clone().equals(x)</pre></blockquote>
+     * will be {@code true}, this is not an absolute requirement.
+     * <p>
+     * By convention, the returned object should be obtained by calling {@code super.clone}.  If a
+     * class and all of its superclasses (except {@code Object}) obey this convention, it will be
+     * the case that {@code x.clone().getClass() == x.getClass()}.
+     * <p>
+     * By convention, the object returned by this method should be independent of this object (which
+     * is being cloned).  To achieve this independence, it may be necessary to modify one or more
+     * fields of the object returned by {@code super.clone} before returning it.  Typically, this
+     * means copying any mutable objects that comprise the internal "deep structure" of the object
+     * being cloned and replacing the references to these objects with references to the copies.  If
+     * a class contains only primitive fields or references to immutable objects, then it is usually
+     * the case that no fields in the object returned by {@code super.clone} need to be modified.
+     * <p>
+     * The method {@code clone} for class {@code Object} performs a specific cloning operation.
+     * First, if the class of this object does not implement the interface {@code Cloneable}, then a
+     * {@code CloneNotSupportedException} is thrown. Note that all arrays are considered to
+     * implement the interface {@code Cloneable} and that the return type of the {@code clone}
+     * method of an array type {@code T[]} is {@code T[]} where T is any reference or primitive
+     * type. Otherwise, this method creates a new instance of the class of this object and
+     * initializes all its fields with exactly the contents of the corresponding fields of this
+     * object, as if by assignment; the contents of the fields are not themselves cloned. Thus, this
+     * method performs a "shallow copy" of this object, not a "deep copy" operation.
+     * <p>
+     * The class {@code Object} does not itself implement the interface {@code Cloneable}, so
+     * calling the {@code clone} method on an object whose class is {@code Object} will result in
+     * throwing an exception at run time.
+     *
+     * @return a clone of this instance.
+     *
+     * @see Cloneable
+     */
+    @Override
+    protected Object clone() {
+        return new FileToBytesExporter(mFilename, mPath, mReadData, mFileSeparator);
+    }
+
+
+    /**
+     * Returns a string representation of the object. In general, the {@code toString} method
+     * returns a string that "textually represents" this object. The result should be a concise but
+     * informative representation that is easy for a person to read. It is recommended that all
+     * subclasses override this method.
+     * <p>
+     * The {@code toString} method for class {@code Object} returns a string consisting of the name
+     * of the class of which the object is an instance, the at-sign character `{@code @}', and the
+     * unsigned hexadecimal representation of the hash code of the object. In other words, this
+     * method returns a string equal to the value of:
+     * <blockquote>
+     * <pre>
+     * getClass().getName() + '@' + Integer.toHexString(hashCode())
+     * </pre></blockquote>
+     *
+     * @return a string representation of the object.
+     */
+    @Override
+    public String toString() {
+        return super.toString();
     }
 
     /**
@@ -428,7 +600,6 @@ public class FileToBytesExporter {
             Visitor<Path> fileVisitor = new Visitor<>(pathMatcher, matches);
             Files.walkFileTree(workingDirectory.toPath(), options, maxDepth, fileVisitor);
             matches.trimToSize();
-//            System.out.println(matches.toString());
             return matches.size() > 0 ? matches : null;
         }
 
@@ -471,8 +642,6 @@ public class FileToBytesExporter {
              */
             @Override
             public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-//                System.out.println("Visited file: " + file.toString());
-//                System.out.println("Is matching? " + mPathMatcher.matches(file.getFileName()));
                 if (mPathMatcher.matches(file.getFileName()) && !attrs.isDirectory())
                     mMatches.add(file.toFile());
                 return FileVisitResult.CONTINUE;
